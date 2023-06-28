da199915a33ef745a6027dbf1f407cc1c535c8bd
vinicius.pena <viniciuspena81@gmail.com>
2023-06-28 16:45:14 -0300

    Inserindo o POST e o GET no app

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // Utilizando a biblioteca EXPRESS para facilitar a criação do código.
 // Utilizando a biblioteca NODEMON para realizar iniciar o servidor sempre que acontecer um save
 const express = require("express");
+const { randomUUID } = require("crypto");
+const { request } = require("http");
 
 // Iniciando a biblioteca
 const app = express();
 
+app.use(express.json());
+
 // Gerando array vazia para servir de banco de dados
-const produto = [];
+const produtos = [];
 
 // Definindo qual a rota retorna determinada mensagem sem if e else
 
@@ -18,6 +22,10 @@ app.get("/primeira-rota", (request, response) => {
 }) 
 ****************************/
 
+// BODY = SEMPRE QUE EU QUISER ENVIAR DADOS PARA MINHA APLICAÇÃO
+// PARAMS = /produtos/1652165123
+// QUERY = /produtos?id=1651561654&values=239237897
+
 // POST = Inserir 
 // GET = Buscar
 // PUT = Alterar
@@ -25,7 +33,23 @@ app.get("/primeira-rota", (request, response) => {
 
 app.post("/produtos", (request, response) => {
     // Nome e preço
-})
+    // Permite que pego o nome e o preço realizando a desestruturação
+    const { nome, preco } = request.body;
+
+    const produto = {
+        nome,
+        preco,
+        id: randomUUID()
+    };
+
+    produtos.push(produto);
+
+    return response.json(produto);
+});
+
+app.get("/produtos", (request, response) => {
+    return response.json(produtos);
+});
 
 // Retorno
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002."));
